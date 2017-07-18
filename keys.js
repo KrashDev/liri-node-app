65b3762a4dc6aaff06207af0d9540dd6f4dbd5ef
Donny Gabai <mrdgabai@gmail.com>
2017-07-17 20:57:27 -0400

    first

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '<KK1vQedR52YuQl9x5PrwajZNZ>',
+  consumer_secret: '<XI9JqJbKnTYILmFYkZGw10L9PJkKExJM089maIaiHQO6LToGYW>',
+  access_token_key: '<887044364731645952-2yV2WeC4fNQtJIpnH2Lj88GwkqPbiiz>',
+  access_token_secret: '<887044364731645952-2yV2WeC4fNQtJIpnH2Lj88GwkqPbiiz>',
+}
---
+++ liri.js
@@ -0,0 +1,6 @@
+var argOne = process.argv[2];
+var argTwo = process.argv[3];
+var argThree = process.argv[4];
+var argFour = process.argv[5];
+
+var Twitter = require('twitter');
